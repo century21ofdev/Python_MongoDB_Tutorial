6c44741f9983ba0e7bdb4805f8cb105dfb671b94
century21ofdev <npng341@gmail.com>
2019-08-27 18:45:01 +0300

    added comment to aggregation pipeline

--- Aggregation/aggregation_pipeline.py
+++ Aggregation/aggregation_pipeline.py
@@ -4,8 +4,16 @@ client = MongoClient('mongodb://localhost:27017/')  # initialize with your URI
 your_collection = client['_books']  # your collection string here
 
 result = your_collection.aggregate([
-    {'$match': {'status': "A"}},
+    {'$match': {'status': "A"}},  # adding first stage with $match function to filter the status field
     {'$group': {'_id': "$b_id", 'total': {'$sum': "$stock"}}}
+    # grouped book_id fields as _id that indicates new total stock field
 ])
 
 print(list(result))
+
+"""
+there is also a few stage type that we can use instead of $group or $match in case our needed
+so that's why i highly recommend you to visit the below reference guide to get more information about that's
+
+https://docs.mongodb.com/manual/reference/operator/aggregation-pipeline/
+"""
