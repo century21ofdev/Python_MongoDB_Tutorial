eea9041bfa2e76aa5239e7b7ec660a830350cc9f
century21ofdev <npng341@gmail.com>
2019-08-27 18:23:37 +0300

    pipeline aggregation updated

--- Aggregation/aggregation_pipeline.py
+++ Aggregation/aggregation_pipeline.py
@@ -4,11 +4,8 @@ client = MongoClient('mongodb://localhost:27017/')  # initialize with your URI
 your_collection = client['_books']  # your collection string here
 
 result = your_collection.aggregate([
-    {'$match':
-         {'$b_id': 101}
-     },
-    {'$group':
-         {'_id': "$b_id", 'total_stock': {"$sum": "$stock"}}
-     }
+    {'$match': {'status': "A"}},
+    {'$group': {'_id': "$b_id", 'total': {'$sum': "$stock"}}}
 ])
 
+print(list(result))
